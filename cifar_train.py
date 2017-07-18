a79bc93e74dbef57a44b3d6c340ed41d6de86250
Randall Smith <agrippa@morpheus.csres.utexas.edu>
2017-07-18 17:52:22 +0000

    migrated to tensorflow

--- cifar_train.py
+++ cifar_train.py
@@ -5,6 +5,7 @@ from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
 from cifar import Cifar
+from keras import backend as K
 
 num_train_images = 50000
 num_test_images = 10000
@@ -18,7 +19,7 @@ c = Cifar(num_train_images , num_test_images)
 # Make Keras happy.
 
 # Reshape and convert to float in range [0, 1] for Keras.
-train_data = train_data.reshape(train_data.shape[0], 3, 32, 32)
+train_data = train_data.reshape(train_data.shape[0], 32, 32, 3)
 train_data = train_data.astype('float32')
 train_data /= 255
 
@@ -34,7 +35,7 @@ print("train_labels = %s" % str(train_labels.shape))
 # OK, do the same for the test data.
 
 # Reshape and convert to float in range [0, 1] for Keras.
-test_data = test_data.reshape(test_data.shape[0], 3, 32, 32)
+test_data = test_data.reshape(test_data.shape[0], 32, 32, 3)
 test_data = test_data.astype('float32')
 test_data /= 255
 
@@ -48,11 +49,11 @@ test_labels = np_utils.to_categorical(test_labels)
 print("test_labels = %s" % str(test_labels.shape))
 
 model = Sequential()
-model.add(Convolution2D(32, 3, 3, activation='relu', input_shape=(3,32,32)))
+model.add(Convolution2D(32, (3, 3), activation='relu', input_shape=(32,32, 3)))
 
 print(model.output_shape)
 
-model.add(Convolution2D(32, 3, 3, activation='relu'))
+model.add(Convolution2D(32, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(0.25))
 
@@ -66,6 +67,6 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 model.fit(train_data, train_labels,
-          batch_size=32, nb_epoch=10, verbose=1)
+	  batch_size=32, epochs=20, verbose=1)
 
 score = model.evaluate(test_data, test_labels, verbose=0)
